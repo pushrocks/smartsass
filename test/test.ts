cfcff789a23c582cd63e228de1e632d3087d67e6
Phil Kunz <github@philkunz.com>
2018-11-28 11:04:26 +0100

    fix(format): run gitzone format

--- test/test.ts
+++ test/test.ts
@@ -5,7 +5,7 @@ import * as smartsass from '../ts/index';
 tap.test('should create a valid instance of smartsass', async () => {
   const testsmartSass = new smartsass.Smartsass({
     data: 'hello'
-  })
+  });
   expect(testsmartSass).to.be.instanceof(smartsass.Smartsass);
 });
 
